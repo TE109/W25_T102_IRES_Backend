98204e899608d0a3da23ceb062a5a59a0f37ef6b
margaretTere <may.terechenko@gmail.com>
2025-01-09 17:04:39 -0500

    Made a database container and tested connection

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 CFG = {
-    PORT: 3000
+    PORT: 3000,
+//    MONGODB_URI: 'mongodb://mongo-ires:27017/ires',
+    MONGODB_URI: 'mongodb://localhost:27017/ires',
+    SECRET: 'LoremIpsumissimplydummytext'
 }
 
 module.exports = CFG;
